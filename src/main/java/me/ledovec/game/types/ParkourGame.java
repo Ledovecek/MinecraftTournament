c1dddf68fd81869f14beac410030ada220877d39
Ledovecek <ledovec.official@gmail.com>
2023-06-15 12:07:47 +0200

    game provider impl

--- src/main/java/me/ledovec/game/types/ParkourGame.java
+++ src/main/java/me/ledovec/game/types/ParkourGame.java
@@ -1,58 +1,7 @@
 package me.ledovec.game.types;
 
-import me.ledovec.events.GameFinishEvent;
-import me.ledovec.events.GamePauseEvent;
-import me.ledovec.events.GameStartEvent;
-import me.ledovec.events.GameUnpauseEvent;
-import me.ledovec.game.Game;
-import me.ledovec.listener.GameListener;
-import me.ledovec.session.GameSession;
-import org.bukkit.Bukkit;
+import me.ledovec.game.GameProvider;
 
-public class ParkourGame implements Game {
-
-    private GameListener gameListener;
-
-    private GameSession gameSession;
-
-    @Override
-    public void start() {
-        Bukkit.getPluginManager().callEvent(new GameStartEvent(this));
-    }
-
-    @Override
-    public void stop() {
-        Bukkit.getPluginManager().callEvent(new GameFinishEvent(this));
-    }
-
-    @Override
-    public void pause() {
-        Bukkit.getPluginManager().callEvent(new GamePauseEvent(this));
-    }
-
-    @Override
-    public void unpause() {
-        Bukkit.getPluginManager().callEvent(new GameUnpauseEvent(this));
-    }
-
-    @Override
-    public void setGameListener(GameListener gameListener) {
-        this.gameListener = gameListener;
-    }
-
-    @Override
-    public void setGameSession(GameSession gameSession) {
-        this.gameSession = gameSession;
-    }
-
-    @Override
-    public GameListener getGameListener() {
-        return gameListener;
-    }
-
-    @Override
-    public GameSession getGameSession() {
-        return gameSession;
-    }
+public class ParkourGame extends GameProvider {
 
 }
